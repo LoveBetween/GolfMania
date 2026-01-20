c53f21ae76165489e46e8626051e375fe2400e0a
unknown <gallupgall@gmail.com>
2026-01-20 22:50:54 +0100

    Some lighting modifications

--- Assets/Scenes/Golfmania_Asset/Scripts/ManuelClub.cs
+++ Assets/Scenes/Golfmania_Asset/Scripts/ManuelClub.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class ManuelClub : MonoBehaviour
 {
   public float moveSpeed = 8f;
+  public float rotationSpeed = 120f;
 
   private Vector3 lastPosition;
   private Vector3 velocity;
+
 
 
   void Start()
@@ -18,8 +20,18 @@ public class ManuelClub : MonoBehaviour
       float h = Input.GetAxis("Horizontal");
       float v = Input.GetAxis("Vertical");
 
-      Vector3 move = new Vector3(h, 0f, v) * moveSpeed * Time.deltaTime;
+      Vector3 move = transform.TransformDirection(new Vector3(h, 0f, v)) * moveSpeed * Time.deltaTime;
       transform.position += move;
+
+      float rotation = 0f;
+
+      if (Input.GetKey(KeyCode.Q))
+          rotation -= 1f;
+
+      if (Input.GetKey(KeyCode.E))
+          rotation += 1f;
+
+        transform.Rotate(Vector3.up, rotation * rotationSpeed * Time.deltaTime);
 
 
       velocity = (transform.position - lastPosition) / Time.deltaTime;
